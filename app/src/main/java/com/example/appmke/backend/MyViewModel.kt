eccd58671fc4704bbb49605bca521f64badaed3a
Emilce Cano <emilce.cano95@gmail.com>
2023-07-28 10:20:29 -0300

    create viewmodelfactory and update

--- app/src/main/java/com/example/appmke/backend/MyViewModel.kt
+++ app/src/main/java/com/example/appmke/backend/MyViewModel.kt
@@ -7,10 +7,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
-class MyViewModel: ViewModel() {
+class MyViewModel(val myRepository:Repository): ViewModel() {
     val data = MutableLiveData<MutableList<Products>>()
 
-    val myRepository = Repository()
+//    val myRepository = Repository()
 
 
     fun requestProduct(products: Products) {
--- app/src/main/java/com/example/appmke/backend/ViewModelFactory.kt
+++ app/src/main/java/com/example/appmke/backend/ViewModelFactory.kt
@@ -3,8 +3,9 @@ package com.example.appmke.backend
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-//class ViewModelFactory ( private val myRepo: Repository): ViewModelProvider.Factory{
-//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-//        return MyViewModel(myRepo) as T
-//    }
-//}
+class ViewModelFactory (private var myRepo: Repository): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MyViewModel(myRepo) as T
+    }
+
+}
