bd8cf647ec2a8a4d86ab7f31a9e08b37ca30b21f
Emilce Cano <emilce.cano95@gmail.com>
2023-07-28 09:46:30 -0300

    update

--- app/src/main/java/com/example/appmke/backend/MyViewModel.kt
+++ app/src/main/java/com/example/appmke/backend/MyViewModel.kt
@@ -8,17 +8,17 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 class MyViewModel: ViewModel() {
-    val data = MutableLiveData<Products?>()
+    val data = MutableLiveData<MutableList<Products>>()
 
     val myRepository = Repository()
 
 
-//    fun requestProduct(products: Products) {
-//        CoroutineScope(Dispatchers.IO).launch {
-//            val result= myRepository.getListProducts()
-//            data.postValue(result)
-//        }
-//    }
+    fun requestProduct(products: Products) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val result= myRepository.getListProducts()
+            data.postValue(result)
+        }
+    }
 
 
 }
