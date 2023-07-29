296815cce7a3d4e4400e98ec702c6c772eecea16
Kevin Gonzalez <kevingonzalezr433@gmail.com>
2023-07-29 15:28:07 -0300

    cambios en la activity

--- app/src/main/java/com/example/appmke/backend/ViewModelFactory.kt
+++ app/src/main/java/com/example/appmke/backend/ViewModelFactory.kt
@@ -3,8 +3,8 @@ package com.example.appmke.backend
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-//class ViewModelFactory ( private val myRepo: Repository): ViewModelProvider.Factory{
-//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-//        return MyViewModel(myRepo) as T
-//    }
-//}
+class ViewModelFactory ( private val myRepo: Repository): ViewModelProvider.Factory{
+  override fun <T : ViewModel> create(modelClass: Class<T>): T {
+      return MyViewModel() as T
+    }
+}
--- app/src/main/java/com/example/appmke/ui/DescriptionActivity.kt
+++ app/src/main/java/com/example/appmke/ui/DescriptionActivity.kt
@@ -1,7 +1,9 @@
 package com.example.appmke.ui
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.appmke.databinding.ActivityDescriptionBinding
 
 class DescriptionActivity : AppCompatActivity() {
@@ -10,5 +12,16 @@ class DescriptionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDescriptionBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
+
+    /* fun buyProduct() {
+        Toast.makeText(this, "Producto comprado", Toast.LENGTH_SHORT).show()
+    }
+    
+    fun init() {
+        binding.bt_buy.setOnClickListener{
+            buyProduct()
+        }
+    }*/
 }
