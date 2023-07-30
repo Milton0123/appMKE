fcb8344fd830a6a000e2318ef00d54650f81652e
Milton0123 <maletta.milton@gmail.com>
2023-07-30 09:42:06 -0300

    solution home

--- app/src/main/java/com/example/appmke/ui/HomeActivity.kt
+++ app/src/main/java/com/example/appmke/ui/HomeActivity.kt
@@ -2,10 +2,11 @@ package com.example.appmke.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModel
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import com.example.appmke.databinding.ActivityHomeBinding
 import com.example.appmke.backend.MyViewModel
+import com.example.appmke.backend.Products
 import com.example.appmke.backend.ProductsAdapter
 import com.example.appmke.backend.Repository
 import com.example.appmke.backend.ViewModelFactory
@@ -13,9 +14,10 @@ import com.example.appmke.backend.ViewModelFactory
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
-
-    private lateinit var viewModel: MyViewModel
     private val myRepository = Repository()
+    private val viewModelFactory = ViewModelFactory(myRepository)
+    private val viewModel = ViewModelProvider(this, viewModelFactory).get(MyViewModel::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -24,17 +26,31 @@ class HomeActivity : AppCompatActivity() {
         action()
     }
 
-    fun action() {
-        initRecyclerView()
+
+    fun action(){
+        initRecyclerView(viewModel.getMyListProduct())
+        searchProduct()
     }
 
-    fun initRecyclerView() {
-        val viewModelFactory = ViewModelFactory(myRepository)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(MyViewModel::class.java)
-
-        val adapter = ProductsAdapter(viewModel.getMyListProduct())
+    fun initRecyclerView(listRv : List<Products>) {
+        val adapter = ProductsAdapter(listRv)
         binding.rvProductsHome.adapter = adapter
     }
 
+    fun searchProduct(){
+        binding.svProductsHome.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val newList = mutableListOf<Products>()
+                    if(it.name.contains(query!!)){
+                        newList.add(it)
+                    }
+                }
+                initRecyclerView(newList)
+                return false
+            }
 
-}
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        }
+    }
