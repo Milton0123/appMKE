5c6f73382b2f4dc0a43c801c8bbb7f363a8830ab
Milton0123 <maletta.milton@gmail.com>
2023-07-30 07:37:00 -0300

    create validation of product

--- app/src/main/java/com/example/appmke/ui/HomeActivity.kt
+++ app/src/main/java/com/example/appmke/ui/HomeActivity.kt
@@ -2,8 +2,10 @@ package com.example.appmke.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
 import com.example.appmke.databinding.ActivityHomeBinding
 import com.example.appmke.backend.MyViewModel
+import com.example.appmke.backend.Products
 import com.example.appmke.backend.ProductsAdapter
 import com.example.appmke.backend.Repository
 
@@ -21,12 +23,32 @@ class HomeActivity : AppCompatActivity() {
     }
 
     fun action(){
-        initRecyclerView()
+        initRecyclerView(miRepository.getListProducts())
+        searhProduct()
     }
 
-    fun initRecyclerView(){
-        val adapter = ProductsAdapter(miRepository.getListProducts())
+    fun initRecyclerView(listRv : List<Products>){
+        val adapter = ProductsAdapter(listRv)
         binding.rvProductsHome.adapter = adapter
+    }
+
+    fun searhProduct(){
+        binding.svProductsHome.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val newList = mutableListOf<Products>()
+                miRepository.getListProducts().forEach{
+                    if(it.name.contains(query!!)){
+                        newList.add(it)
+                    }
+                }
+                initRecyclerView(newList)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
     }
 
 
